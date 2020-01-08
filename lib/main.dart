d6615cfc18df282de40432aa9f3dd01312c962d0
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-01-08 16:10:20 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Center(
+          child: Text('I AM Rich ww3 Edition'),
+        ),
+      ),
+    );
